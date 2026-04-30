9ac8a09bb0290faf9e0f364fbbda1a22cf59b4dc
Mark Vletter <mark.vletter@voys.nl>
2026-04-30 21:27:27 +0200

    style: format merge migration with ruff

--- klai-portal/backend/alembic/versions/71f4add474f4_merge_spec_auth_009_with_pending_.py
+++ klai-portal/backend/alembic/versions/71f4add474f4_merge_spec_auth_009_with_pending_.py
@@ -5,11 +5,12 @@ Revises: a1b2c3d4e5f6, v2m3e4r5g6h7
 Create Date: 2026-04-30 21:21:24.575762
 
 """
+
 from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
-revision: str = '71f4add474f4'
-down_revision: Union[str, Sequence[str], None] = ('a1b2c3d4e5f6', 'v2m3e4r5g6h7')
+revision: str = "71f4add474f4"
+down_revision: Union[str, Sequence[str], None] = ("a1b2c3d4e5f6", "v2m3e4r5g6h7")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
